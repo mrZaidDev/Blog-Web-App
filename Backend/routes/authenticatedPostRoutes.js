7d0f67fd1c4b400953323f09e46e45dad79f7e64
Zaid Khan <zaidcodingtopcoding@gmail.com>
2025-08-02 11:36:09 +0500

    small changes

--- Backend/routes/authenticatedPostRoutes.js
+++ Backend/routes/authenticatedPostRoutes.js
@@ -2,7 +2,7 @@ import express from 'express'
 import {postingPost,updatingPost,deletingPost} from '../controllers/postController.js'
 const router = express.Router()
 
-router.post('/', postingPost)
+// router.post('/', postingPost)
 router.put('/:id',updatingPost)
 router.delete('/:id',deletingPost)
 router.get('/',(req,res) => {
