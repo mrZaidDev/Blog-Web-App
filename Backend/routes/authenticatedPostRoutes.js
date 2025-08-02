2f59e4ae1bf57d5ae64cc087eda63480aca1535c
Zaid Khan <zaidcodingtopcoding@gmail.com>
2025-08-02 11:35:28 +0500

    small changes

--- Backend/routes/authenticatedPostRoutes.js
+++ Backend/routes/authenticatedPostRoutes.js
@@ -5,6 +5,8 @@ const router = express.Router()
 router.post('/', postingPost)
 router.put('/:id',updatingPost)
 router.delete('/:id',deletingPost)
-
+router.get('/',(req,res) => {
+    res.json({message:"It works if there is no db uses uri"})
+})
 
 export default router
